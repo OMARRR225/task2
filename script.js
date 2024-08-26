d13a09ca17da8d81f5848ef0a5a5152bf3f42170
unknown <OMAR>
2024-08-26 13:06:53 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+/*
+const myJsonObjectFromServer='{"Username":"Osama","Age":39}';
+console.log(typeof myJsonObjectFromServer);
+console.log(myJsonObjectFromServer);
+
+const myJsObject = JSON.parse(myJsonObjectFromServer);
+console.log(typeof myJsObject);
+console.log(myJsObject);
+
+myJsObject["Username"]="Elzero";
+myJsObject["Age"]= 40;
+
+
+const myJsonObjectToServer =JSON.stringify(myJsObject);
+console.log(typeof myJsonObjectToServer);
+console.log(myJsonObjectToServer);
+
+*/
+
+
+console.log("1");
+console.log("2");
+setTimeout(()=> console.log("Operation"),4000);
+console.log("3"); 
+
+
+
+
+
+
+
+
+
+
+
